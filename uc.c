91ce68d0c17e3f477e265175bb727642cb84c34c
Sergey Veneckiy <Sergey_Veneckiy@epam.com>
2024-05-30 14:50:14 +0400

    uc.c file refactoring

--- uc.c
+++ uc.c
@@ -3,25 +3,31 @@
 #include <string.h>
 #include "tinyexpr.h"
 
+// Function to print an error message
 void print_error() {
     printf("Error. Incorrect expression.\n");
 }
 
 int main(int argc, char *argv[]) {
+    // Check if command-line arguments are provided
     if (argc > 1) {
         // Handle command-line arguments for power function...
     } else {
         char exp[256];
+
+        // Main calculator loop
         while (1) {
+            // Prompt user for expression
             printf("Expression: ");
             fgets(exp, 256, stdin);
             exp[strcspn(exp, "\n")] = 0; // Remove newline character
 
+            // Check if user wants to quit
             if (strcmp(exp, "q") == 0) {
                 break;
             }
 
-            // Remove spaces
+            // Remove spaces from the expression
             char *ptr = exp;
             while (*ptr) {
                 if (*ptr == ' ') {
@@ -31,21 +37,23 @@ int main(int argc, char *argv[]) {
                 }
             }
 
+            // Evaluate the expression using tinyexpr library
             int err;
             double result = te_interp(exp, &err);
+
+            // Check for evaluation errors
             if (err != 0) {
                 print_error();
             } else {
-
- // Check if result is an integer
-    if (result == floor(result)) {
-        printf("Result: %.0f\n\n", result); // Print with no decimal places
-    } else {
-        printf("Result: %.10f\n\n", result); // Print with 10 decimal places
-    }
-
+                // Check if result is an integer
+                if (result == floor(result)) {
+                    printf("Result: %.0f\n\n", result); // Print with no decimal places
+                } else {
+                    printf("Result: %.10f\n\n", result); // Print with 10 decimal places
+                }
             }
         }
     }
+
     return 0;
 }
