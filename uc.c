37dcda02d6aa4265b4ba7a1db87b24d8b5ea5d85
Sergey Veneckiy <Sergey_Veneckiy@epam.com>
2024-06-01 19:46:00 +0400

    add support of readline lib and add LICENSE file

--- uc.c
+++ uc.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "conv_str_to_math_res.h"
 
 int main(int argc, char *argv[]) {
@@ -8,22 +10,29 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         // Handle command-line arguments for power function...
     } else {
-        char exp[256];
+        char *exp;
         // Main calculator loop
         while (1) {
-            // Prompt user for expression
-            printf("Expression: ");
-            fgets(exp, 256, stdin);
-            exp[strcspn(exp, "\n")] = 0; // Remove newline character
+            // Prompt user for expression using readline
+            exp = readline("Expression: ");
+            if (exp == NULL) {
+                break;
+            }
+
+            // Add the expression to the history
+            add_history(exp);
 
             // Check if user wants to quit
             if (strcmp(exp, "q") == 0) {
+                free(exp);
                 break;
             }
+
             long double result = parseMathExpression(exp);
             char *formattedResult = formatResult(result);
             printf("Result: %s\n", formattedResult);
 
+            free(exp);
         }
     }
     return 0;
