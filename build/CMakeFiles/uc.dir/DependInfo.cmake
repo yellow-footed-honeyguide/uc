7e413ed6946ed69bb1b013fc333b44d5e12b1783
Sergey Veneckiy <Sergey_Veneckiy@epam.com>
2024-06-02 21:30:33 +0400

    the ability to raise a number to a power has been added

---
+++ build/CMakeFiles/uc.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/honeyguide/Desktop/uc/conv_str_to_math_res.c" "CMakeFiles/uc.dir/conv_str_to_math_res.c.o" "gcc" "CMakeFiles/uc.dir/conv_str_to_math_res.c.o.d"
+  "/home/honeyguide/Desktop/uc/handle_arguments.c" "CMakeFiles/uc.dir/handle_arguments.c.o" "gcc" "CMakeFiles/uc.dir/handle_arguments.c.o.d"
+  "/home/honeyguide/Desktop/uc/power_uc.c" "CMakeFiles/uc.dir/power_uc.c.o" "gcc" "CMakeFiles/uc.dir/power_uc.c.o.d"
+  "/home/honeyguide/Desktop/uc/uc.c" "CMakeFiles/uc.dir/uc.c.o" "gcc" "CMakeFiles/uc.dir/uc.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ power_uc.h
@@ -0,0 +1,6 @@
+#ifndef POWER_UC_H
+#define POWER_UC_H
+
+long double power_uc(long double base, long double exponent);
+
+#endif
